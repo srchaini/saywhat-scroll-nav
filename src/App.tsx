d995e2784201202a590870de456a5697ba53cee9
srchaini <srchaini.sachetana@gmail.com>
2025-10-01 18:26:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/saywhat-scroll-nav/">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* add other routes here */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
